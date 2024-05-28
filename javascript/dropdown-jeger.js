ceae2373f0429ef1d92abd27e580f98e9a058cab
Freyaui <freya.sostack@gmail.com>
2024-05-28 08:36:57 +0200

    freyas javascript

---
+++ javascript/dropdown-jeger.js
@@ -0,0 +1,31 @@
+console.log("please virk")
+
+let dropdown = document.getElementById("title-dropdown");
+let titles = [
+    "Direktør",
+    "Ejer",
+    "Konsulent",
+    "Manager",
+    "Medarbejder",
+    "Studerende",
+    "Top Executive (formand, administrerende direktør osv.)",
+    "Vice President /VP /EVP",
+    "Andet"
+];
+
+ for (let i = 0; i < titles.length; i++) {
+    let option = document.createElement("option");
+    option.text = titles[i];
+    option.value = titles[i];
+    dropdown.add(option);
+}
+
+dropdown.addEventListener("change", function() {
+    if (this.value !== "") {
+        this.blur();
+        let selectedTitle = this.value;
+        console.log("Du har valgt: " + selectedTitle);
+    } else {
+        console.log("Ingen titel valgt.");
+    }
+});
