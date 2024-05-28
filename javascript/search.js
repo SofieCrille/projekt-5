143047673ad208086626e48f95997e71f3e90cff
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2024-05-28 21:29:02 +0200

    1

--- javascript/search.js
+++ javascript/search.js
@@ -3,12 +3,15 @@ function showDropdown() {
 }
 
 function filterDropdown() {
+
   let input = document.getElementById("input");
   let filter = input.value.toUpperCase();
   let div = document.getElementById("dropdown-searchbar");
   let a = div.getElementsByTagName("a");
+
   for (let i = 0; i < a.length; i++) {
     let txtValue = a[i].textContent || a[i].innerText;
+    
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       a[i].style.display = "";
     } else {
